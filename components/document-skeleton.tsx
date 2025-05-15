907b2181c1bead4c70a207474d9c2294043f2b40
ejjays <christsonalloso021@gmail.com>
2025-05-16 01:26:39 +0800

    ..

--- components/document-skeleton.tsx
+++ components/document-skeleton.tsx
@@ -7,11 +7,29 @@ export const DocumentSkeleton = ({
 }: {
   artifactKind: ArtifactKind;
 }) => {
-  return artifactKind === 'image' ? (
-    <div className="flex flex-col gap-4 w-full justify-center items-center h-[calc(100dvh-60px)]">
-      <div className="animate-pulse rounded-lg bg-muted-foreground/20 size-96" />
+  return artifactKind === 'code' ? (
+    <div className="flex flex-col gap-4 w-full">
+      <div className="animate-pulse rounded-lg h-12 bg-muted-foreground/20 w-1/2" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-full" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-full" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-1/3" />
+      <div className="animate-pulse rounded-lg h-5 bg-transparent w-52" />
+      <div className="animate-pulse rounded-lg h-8 bg-muted-foreground/20 w-52" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-2/3" />
+    </div>
+  ) : artifactKind === 'sheet' ? (
+    // Show sheet skeleton
+    <div className="flex flex-col gap-4 w-full">
+      <div className="animate-pulse rounded-lg h-12 bg-muted-foreground/20 w-1/2" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-full" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-full" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-1/3" />
+      <div className="animate-pulse rounded-lg h-5 bg-transparent w-52" />
+      <div className="animate-pulse rounded-lg h-8 bg-muted-foreground/20 w-52" />
+      <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-2/3" />
     </div>
   ) : (
+    // Default text skeleton
     <div className="flex flex-col gap-4 w-full">
       <div className="animate-pulse rounded-lg h-12 bg-muted-foreground/20 w-1/2" />
       <div className="animate-pulse rounded-lg h-5 bg-muted-foreground/20 w-full" />
